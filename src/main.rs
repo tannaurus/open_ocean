ec2acf29ea46ae0bfe5b5433a467218942647997
Tanner Gill <contacttannergill@gmail.com>
2023-07-23 13:01:47 -0700

    Can pause the game! No ui, yet.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::time::Duration;
 
 mod components;
 use components::{id, ship};
+mod ui;
 
 fn main() {
     App::new()
@@ -14,9 +15,10 @@ fn main() {
             watch_for_changes: ChangeWatcher::with_delay(Duration::from_millis(200)),
             ..default()
         }))
+        // Physics plugins
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
-        // water plugins
+        // Water plugins
         .add_plugins(WaterPlugin)
         .add_plugins(ImageUtilsPlugin)
         .add_plugins(AtmospherePlugin)
@@ -25,28 +27,70 @@ fn main() {
             amplitude: 2.0,
             ..Default::default()
         })
-        // core game plugins
-        .add_plugins(Game)
+        // Core game plugins
+        .add_state::<ui::MenuState>()
+        .add_plugins(Ui)
+        .add_plugins(GameMechanics)
         .run();
 }
 
-pub struct Game;
-
-impl Plugin for Game {
+pub struct GameMechanics;
+impl Plugin for GameMechanics {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, lights)
             .add_systems(Startup, world)
             .add_systems(Startup, ship::Systems::spawn_ship)
-            .add_systems(Update, ship::Systems::movement)
-            .add_systems(Update, ship::Systems::camera)
-            .add_systems(Update, ship::Systems::cannons)
-            .add_systems(Update, quit);
+            .add_systems(
+                Update,
+                ship::Systems::movement.run_if(state_exists_and_equals(ui::MenuState::Ship)),
+            )
+            .add_systems(
+                Update,
+                ship::Systems::camera.run_if(state_exists_and_equals(ui::MenuState::Ship)),
+            )
+            .add_systems(
+                Update,
+                ship::Systems::cannons.run_if(state_exists_and_equals(ui::MenuState::Ship)),
+            );
     }
 }
 
-fn quit(keyboard: Res<Input<KeyCode>>, mut exit: EventWriter<AppExit>) {
+pub struct Ui;
+impl Plugin for Ui {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, toggle_pause_menu);
+    }
+}
+
+// fn quit(keyboard: Res<Input<KeyCode>>, mut exit: EventWriter<AppExit>) {
+//     if keyboard.just_pressed(KeyCode::Escape) {
+//         exit.send(AppExit);
+//     }
+// }
+
+fn toggle_pause_menu(
+    mut commands: Commands,
+    keyboard: Res<Input<KeyCode>>,
+    game_state: Res<State<ui::MenuState>>,
+    mut next_game_state: ResMut<NextState<ui::MenuState>>,
+) {
     if keyboard.just_pressed(KeyCode::Escape) {
-        exit.send(AppExit);
+        let updated_state = ui::MenuState::toggle_pause(game_state.get());
+        match updated_state {
+            ui::MenuState::Pause => {
+                commands.spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        justify_content: JustifyContent::SpaceBetween,
+                        ..default()
+                    },
+                    ..default()
+                });
+            }
+            _ => {}
+        }
+        next_game_state.set(updated_state);
     }
 }
 
